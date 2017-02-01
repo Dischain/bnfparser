effc4800b249630a37f900303c202d2a4018ac8c
Aleksands Shaytanov <rdischain@yandex.ru>
2017-02-01 23:25:37 +0400

    Add InitialRule() setters and getters

--- src/main/java/com/dischain/bnfparser/BNFContents/BNFGrammar.java
+++ src/main/java/com/dischain/bnfparser/BNFContents/BNFGrammar.java
@@ -2,20 +2,54 @@ package com.dischain.bnfparser.BNFContents;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BNFGrammar {
 
     private HashMap<String, AbstractBNFRule> rules;
 
-    public BNFGrammar(List<AbstractBNFRule> rules) {
+    private Map<String, NonterminalMLVariable> nontermAlphabet;
+
+    private AbstractBNFRule initialRule;
+
+    public BNFGrammar (List<AbstractBNFRule> rules) {
         this.rules = new HashMap<String, AbstractBNFRule>();
         for (AbstractBNFRule rule : rules) {
             this.rules.put(rule.getRuleName(), rule);
         }
+        setNonermAlphabet();
+    }
+
+    private void setNonermAlphabet() {
+        nontermAlphabet = new HashMap<String, NonterminalMLVariable>();
+        for (String nonTerm : rules.keySet()) {
+            nontermAlphabet.put(nonTerm, new NonterminalMLVariable(nonTerm));
+        }
+    }
+
+    public BNFGrammar (List<AbstractBNFRule> rules, String initial) {
+        this(rules);
+        initialRule = getRule(initial);
+    }
+
+    public void setInitialRule (String initial) {
+        this.initialRule = getRule(initial);
+    }
+
+    public AbstractBNFRule getInitialRule () {
+        return initialRule;
     }
 
     public AbstractBNFRule getRule (String ruleName) {
         return rules.get(ruleName);
+    }
+
+    public Map<String, NonterminalMLVariable> getNontermAlphabet () {
+        return nontermAlphabet;
+    }
+
+    public AbstractMLVariable getNonterm (String name) {
+        return nontermAlphabet.get(name);
     }
 
     public HashMap<String, AbstractBNFRule> getRules () {
