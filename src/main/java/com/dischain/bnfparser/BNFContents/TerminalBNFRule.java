f8cfc7555a8c7a8518cf4ab332cc1f4a264e54c6
Aleksands Shaytanov <rdischain@yandex.ru>
2017-02-07 23:15:58 +0400

    Refactored

--- src/main/java/com/dischain/bnfparser/BNFContents/TerminalBNFRule.java
+++ src/main/java/com/dischain/bnfparser/BNFContents/TerminalBNFRule.java
@@ -6,6 +6,30 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Реализация терминального правила КС-грамматики. Иными словами
+ * данное правило может определять правила вывода только для терминальных
+ * символов КС-грамматики.
+ * <p>Например, данное правило является терминальным:</p>
+ * <p><code>RULE = "term1" | "tern2" | "term3"</code></p>
+ * Может содержать множество только выражений, являющимхся терминальными
+ * ({@link TerminalBNFExpression}).
+ *
+ * <p>Так как данная реализация парсера по КС-грамматике предоставляет
+ * терминальные выражения, состоящие только из одного терминального символа,
+ * определение терминального правила не может быть записано в следующе форме:</p>
+ * <p><code>RULE = "term1" "term2" | "term3" term4"</code></p>
+ * <p> - данный API попросту не позволит этого сделать и выбросит
+ * {@link com.dischain.bnfparser.Exception.BNFParseException}
+ *
+ * <p>Терминальное правило содержит в себе {@link TrieTree} - префиксное
+ * дерево, позволяющее определять наличие некоторой цепочки в множестве
+ * терминалов, задаваемом данным правилом, за {@code O(n)}, где {@code n} -
+ * длина указанной цепочки. </p>
+ * 
+ * @see TerminalBNFExpression
+ * @see TerminalMLVariable
+ */
 public class TerminalBNFRule extends AbstractBNFRule {
 
     private TrieTree<String> trieTree;
@@ -18,6 +42,13 @@ public class TerminalBNFRule extends AbstractBNFRule {
         super(ruleName);
     }
 
+    /**
+     * Строит терминальное правило по заданному имени правила и множеству выражений
+     * периодической длины
+     *
+     * @param ruleName имя правила
+     * @param expressions список выражений правила
+     */
     public TerminalBNFRule(String ruleName, TerminalBNFExpression ... expressions) {
         super(ruleName);
         this.expressions.addAll(Arrays.asList(expressions));
@@ -25,6 +56,12 @@ public class TerminalBNFRule extends AbstractBNFRule {
         constructTrieTree(expressions);
     }
 
+    /**
+     * Строит терминальное правило по заданному имени правила и списку его выражений
+     *
+     * @param ruleName имя правила
+     * @param expressions список выражений правила
+     */
     public TerminalBNFRule(String ruleName, List<AbstractBNFExpression> expressions) {
         super(ruleName);
         for (AbstractBNFExpression expr : expressions) {
@@ -35,6 +72,12 @@ public class TerminalBNFRule extends AbstractBNFRule {
         constructTrieTree(expressions);
     }
 
+    /**
+     * Строит префиксное дерево {@link TrieTree} по множеству указанных выражений,
+     * входящих в состав данного правила
+     *
+     * @param expressions список выражений, составляющих данное правило
+     */
     private void constructTrieTree(List<AbstractBNFExpression> expressions) {
         if (trieTree == null)
             trieTree = new TrieTree<String>();
@@ -44,6 +87,12 @@ public class TerminalBNFRule extends AbstractBNFRule {
         }
     }
 
+    /**
+     * Строит префиксное дерево {@link TrieTree} по множеству указанных терминалов,
+     * входящих в состав данного правила
+     *
+     * @param expressions множество терминалов, определяемых данным правилом
+     */
     private void constructTrieTree(AbstractBNFExpression ... expressions) {
         if (trieTree == null)
             trieTree = new TrieTree<String>();
@@ -53,12 +102,20 @@ public class TerminalBNFRule extends AbstractBNFRule {
         }
     }
 
-    public TrieTree<String> getTrieTree () {
+    private TrieTree<String> getTrieTree () {
         if (trieTree == null)
             trieTree = new TrieTree<String>();
         return trieTree;
     }
 
+    /**
+     * Добавляет выражение к терминальному правилу грамматики. В случае, если
+     * выражение не является терминальным, выражение отвергается. В противном
+     * случае выражение добавится к правилу и обновится префиксное дерево.
+     *
+     * @param expressions выражения, добавляемые к правилу. Может содержать периодическое
+     * количество аргументов.
+     */
     public void addExpressions(AbstractBNFExpression... expressions) {
         for (AbstractBNFExpression expr : expressions) {
             if (expr.isTerminalExpression())
@@ -67,10 +124,22 @@ public class TerminalBNFRule extends AbstractBNFRule {
         constructTrieTree(expressions);
     }
 
+    /**
+     * Проверяет, является ли данное правило терминальным
+     *
+     * @return {@code true}, если правило терминальное, {@code false}
+     * в противном случае.
+     */
     public boolean isTerminalRule() {
         return true;
     }
 
+    /**
+     * Возвращает двумерный массив терминальных символов данного правила
+     * в виде множества множеств терминалов.
+     *
+     * @return множество множеств терминалов данного правила.
+     */
     public List<List<AbstractMLVariable>> getListsOfVariables() {
         ArrayList<List<AbstractMLVariable>> listOfVars =
                 new ArrayList<List <AbstractMLVariable>>();
@@ -81,6 +150,12 @@ public class TerminalBNFRule extends AbstractBNFRule {
         return listOfVars;
     }
 
+    /**
+     * Проверяет наличие терминального символа в терминальном правиле.
+     * @param term терминальная последовательность
+     * @return {@code true} если данная терминальная последовательность содержится
+     * в данном правиле, {@code false}, если нет
+     */
     public boolean containsTerm(String term) {
         return trieTree.contains(term);
     }
@@ -95,13 +170,5 @@ public class TerminalBNFRule extends AbstractBNFRule {
             flag = false;
         }
         return result;
-    }
-
-    public static void main(String[] args) {
-        TerminalBNFRule rule = new TerminalBNFRule("STR", new TerminalBNFExpression(new TerminalMLVariable("asda")), new TerminalBNFExpression(new TerminalMLVariable("asdanya")));
-        //rule.addExpressions(new TerminalBNFExpression(new TerminalMLVariable("asda")), new TerminalBNFExpression(new TerminalMLVariable("asda")));
-        //rule.setRuleName("SPP");
-        System.out.println(rule.getListsOfVariables());
-        System.out.println(rule.getTrieTree().get("asd"));
     }
 }
