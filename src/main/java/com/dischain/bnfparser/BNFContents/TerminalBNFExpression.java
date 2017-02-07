a7b975fd5b277f2b1b658f3e61ff1549af4410f7
Aleksands Shaytanov <rdischain@yandex.ru>
2017-02-07 23:17:36 +0400

    Refactored

--- src/main/java/com/dischain/bnfparser/BNFContents/TerminalBNFExpression.java
+++ src/main/java/com/dischain/bnfparser/BNFContents/TerminalBNFExpression.java
@@ -3,7 +3,23 @@ package com.dischain.bnfparser.BNFContents;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * Реализация терминального выражения КС-грамматики.
+ * Терминальные выражения составляют основу терминальных правил
+ * {@link TerminalBNFRule} и содержат не более чем одну терминальную
+ * переменную {@link TerminalMLVariable}.
+ *
+ * <p>Пример правильного терминального выражения:
+ * ["term"]. Тогда, руководствуясь такой нотацией, терминальное правило
+ * можно определить следующим образом:</p>
+ * <p><code>TERM_RULE = {["tern1"], ["term2"], ["term3]}</code></p>
+ *
+ * <p>Соответственно огда неправильное терминальное выражение
+ * можно условно описать так: <code>["term1" "term2" ... "termn"]</code></p>
+ *
+ * @see TerminalBNFRule
+ * @see TerminalMLVariable
+ */
 public class TerminalBNFExpression extends AbstractBNFExpression {
 
     private static int SIZE = 1;
@@ -12,10 +28,14 @@ public class TerminalBNFExpression extends AbstractBNFExpression {
         this.variables = new ArrayList<AbstractMLVariable>(SIZE);
     }
 
+    /**
+     * Строит терминальное выражение по заданной металингвистической переменной.
+     *
+     * @param variable металингвистическая переменная
+     */
     public TerminalBNFExpression (AbstractMLVariable variable) {
         this();
-        if (variable.isTerminal())
-            this.variables.add(variable);
+        addVariable(variable);
     }
 
     public TerminalBNFExpression (TerminalMLVariable variable) {
@@ -28,10 +48,13 @@ public class TerminalBNFExpression extends AbstractBNFExpression {
     }
 
     /**
-     * Добавляет терминальную металингвистическую переменную в выражение.
+     * Добавляет металингвистическую переменную в выражение.
+     * Перед вставкой выполняется проверка перемнной на принадлежность к классу
+     * терминальных переменных
      * Так как терминальное выражение может содержать только одну переменную,
      * при добавлении переменной в уже заполненное выражение старое значение
      * стирается и добавляется новове.
+     *
      * @param variable металингвистическая переменная, которая будет добавлена
      */
     public void addVariable(AbstractMLVariable variable) {
@@ -49,6 +72,14 @@ public class TerminalBNFExpression extends AbstractBNFExpression {
         return true;
     }
 
+    /**
+     * Проверяет на равенство заданного терминала переменной данного терминального
+     * выражения
+     *
+     * @param term указываемая для сравнения цепочка
+     * @return {@code true} если {@code term} равен содержимому данного выражения,
+     * {@code false} в обратном случае
+     */
     public boolean equalsTerm(String term) {
         return variables.get(0).getVariable().equals(term);
     }
